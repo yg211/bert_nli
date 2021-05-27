bb2a059351e69641d080d9cfacb813ef26ab93de
yg211 <gaoyang_alex@hotmail.com>
2021-05-27 15:59:05 +0100

    merge train and dev

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -37,12 +37,12 @@ class CIFAR10(VisionDataset):
         ['data_batch_1', 'c99cafc152244af753f735de768cd75f'],
         ['data_batch_2', 'd4bba439e000b95fd0a9bffe97cbabec'],
         ['data_batch_3', '54ebc095f3ab1f0389bbae665268c751'],
-        ['data_batch_4', '634d18415352ddfa80567beed471001a']  
-        # ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'], # for generalisation test
+        ['data_batch_4', '634d18415352ddfa80567beed471001a'],
+        ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'], # for generalisation test
     ]
 
     dev_list = [
-        ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'], # for generalisation test
+        ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'] # for generalisation test
     ]
 
     test_list = [
